0ab8f28febc0910f1085e02e83ebf88b190b30cc
uiejin <rabbit234@gmail.com>
2021-05-10 19:00:49 +0900

    develop template

--- serv.c
+++ serv.c
@@ -118,7 +118,7 @@ void http_handler(int asock) {
     struct stat st;
 
     strcpy(safe_uri, uri);
-    if (!strcmp(safe_uri, "/")) strcpy(safe_uri, "/index.html");
+    if (!strcmp(safe_uri, "/")) strcpy(safe_uri, "/index2.html");
     
     local_uri = safe_uri + 1;
     if (stat(local_uri, &st) < 0) {
